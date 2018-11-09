99d181840656ad515891978e86bd4b860a2ddcb4
SebastianFrench <44885618+SebastianFrench@users.noreply.github.com>
2018-11-08 21:45:40 -0500

    V1

---
+++ Bolos.java
@@ -0,0 +1,19 @@
+package bolos;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author Sebastián leon
+ *          Sebastian Soracipa
+ */
+public class Bolos {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        Scanner Sc = new Scanner(System.in);
+    }
+    
+}
