e271149c6583866ea580cd16004fe59c47087989
SebastianFrench <44885618+SebastianFrench@users.noreply.github.com>
2018-11-09 12:37:33 -0500

    V2

--- Bolos.java
+++ Bolos.java
@@ -9,11 +9,30 @@ import java.util.Scanner;
  */
 public class Bolos {
 
+    public static void pantalla(int player1, int player2){//leon
+    
+    
+    
+    
+    }
+    public static void logicaPuntuacion(){
+    
+    
+    
+    }
+    public static void cuadro(){
+        System.out.println("-----------------------------------------");
+    
+    
+    }
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
         Scanner Sc = new Scanner(System.in);
+        int player1,player2;
+        cuadro();
+        
     }
     
 }
