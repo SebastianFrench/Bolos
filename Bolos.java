b5fafad5273ea0c17eea490a5a491b9607f127a0
SebastianFrench <44885618+SebastianFrench@users.noreply.github.com>
2018-11-15 11:15:19 -0500

    v3
    falta parte lógica

--- Bolos.java
+++ Bolos.java
@@ -8,31 +8,45 @@ import java.util.Scanner;
  *          Sebastian Soracipa
  */
 public class Bolos {
-
+    static Scanner sc = new Scanner(System.in);
+    static String name1 = null;
+    static String name2 = null;
     public static void pantalla(int player1, int player2){//leon
-    
+        System.out.println("***************Puntuación***************");
+        System.out.println("**********************************************\n*"+name1+"       *        *        *        *        *\n*        *        *        *        *        *\n**********************************************");
+        System.out.println("**********************************************\n*"+name2+"       *        *        *        *        *\n*        *        *        *        *        *\n**********************************************");
     
     
     
     }
     public static void logicaPuntuacion(){
-    
-    
-    
-    }
-    public static void cuadro(){
-        System.out.println("-----------------------------------------");
-    
-    
-    }
-    /**
-     * @param args the command line arguments
-     */
-    public static void main(String[] args) {
-        Scanner Sc = new Scanner(System.in);
-        int player1,player2;
-        cuadro();
         
+        //  Crack
+    
+    
+    
+    }
+    public static void imprimirPantalla(){
+        System.out.println("***************Puntuación***************");
+        for (int i = 0; i < 5; i++) {
+            for (int j = 0; j < 50; j++) {
+                if(i%2 == 0) {
+                    System.out.print("─");
+                } else {
+                    System.out.print("");
+                }
+                
+            System.out.println("|");
+        }
+    }
     }
     
-}
+    public static void main(String[] args) {
+        int player1=0,player2=0;
+        System.out.println("Bienvenidos a bolos");
+        System.out.println("Por favor ingrese los nombres: ");
+        name1 = sc.next();
+        name2 = sc.next();
+        //imprimirPantalla();
+        pantalla(player1, player2);
+    }
